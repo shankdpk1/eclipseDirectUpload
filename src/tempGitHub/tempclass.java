1a918305b4c896250632e2a72e22c2a815dc3e04
shankdpk1 <33471809+shankdpk1@users.noreply.github.com>
2018-01-21 01:45:00 -0500

    Update tempclass.java

--- src/tempGitHub/tempclass.java
+++ src/tempGitHub/tempclass.java
@@ -3,7 +3,7 @@ package tempGitHub;
 public class tempclass {
 
 	public static void main(String[] args) {
-		System.out.println("hikkkkk edited on github");
+		System.out.println("hikkkkk edited on github -000");
 
 	}
 
